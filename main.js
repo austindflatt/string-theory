e9831210c4fbd7c0a508e9e4efe53a32d4b9f0f3
Austin Flatt <austindflatt@gmail.com>
2021-09-29 12:51:12 -0400

    task 1 complete

---
+++ main.js
@@ -0,0 +1,13 @@
+// Task 1 //
+function xify(str) {
+    let output = '';
+    for (let i = 0; i < str.length; i++) {
+    output = output + 'x';
+    }
+    return output;
+}
+  
+  console.log(xify('hello'));
+  console.log(xify('hi there'));
+
+  // Task 2 //
